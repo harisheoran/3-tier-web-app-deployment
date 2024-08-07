64d9126e1ad52b4e74f481ba2085d44d1ff004e5
harisheoran <harisheoran01@gmail.com>
2024-08-07 19:43:12 +0530

    frontend package json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000, // you can replace this port with any port
-    origin: "http://0.0.0.0:3000",
   }
 })
