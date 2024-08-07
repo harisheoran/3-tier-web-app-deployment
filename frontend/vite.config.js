a99cf942b385f2fc4a423688beb090eb4b879a51
harisheoran <harisheoran01@gmail.com>
2024-08-07 18:49:55 +0530

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000, // you can replace this port with any port
+    origin: "http://0.0.0.0:3000"
   }
 })
