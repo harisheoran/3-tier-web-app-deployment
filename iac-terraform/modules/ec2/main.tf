661a407c9912af42d1efaf1c2376086936d8faa6
harisheoran <harisheoran01@gmail.com>
2024-08-08 22:12:04 +0530

    added cd piplines

--- iac-terraform/modules/ec2/main.tf
+++ iac-terraform/modules/ec2/main.tf
@@ -34,8 +34,15 @@ resource "aws_security_group" "prod_aws_security_group" {
     }
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
