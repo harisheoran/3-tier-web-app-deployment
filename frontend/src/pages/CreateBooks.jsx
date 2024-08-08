f2b9e7cea2d0d58360f3a824f06d9cf6bd1d7c2e
harisheoran <harisheoran01@gmail.com>
2024-08-08 09:24:56 +0530

    change frontebnd route

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('/books', data)
+      .post('/api', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
